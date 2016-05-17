97237c9fdc01cdcf3e7830b944f9851837c47d9d
Alex <lyosha85@gmail.com>
2016-05-18 00:23:05 +0900

    Implement 10..20

--- lib/number_in_words.rb
+++ lib/number_in_words.rb
@@ -1,7 +1,14 @@
 class NumberInWords
-  ZEROTOTEN = %w[zero one two three four five six seven eight nine ten]
+  ZEROTOTWENTY = %w[zero one two three four five six seven eight nine ten
+                      eleven twelve thirteen fourteen fifteen sixteen seventeen
+                      eighteen nineteen twenty]
   def convert(number)
     number = Integer(number)
-    ZEROTOTEN[number]
+    case number
+      when 0..20
+        ZEROTOTWENTY[number]
+      else
+        "hai"
+    end
   end
 end
--- spec/number_in_words_spec.rb
+++ spec/number_in_words_spec.rb
@@ -3,16 +3,16 @@ require 'number_in_words'
 
 describe NumberInWords, '#convert' do
   subject(:number_in_words) { NumberInWords.new }
-  it "converts" do
     {
       "1"   => "one",
       "3"   => "three",
       "6"   => "six",
       "10"   => "ten",
       "11"  => "eleven",
+      "14"  => "fourteen",
+      "19"  => "nineteen",
       "100" => "one hundred",
     }.each do |input, result|
-      expect(number_in_words.convert input).to eq result
+      it { expect(number_in_words.convert input).to eq result }
     end
-  end
 end
