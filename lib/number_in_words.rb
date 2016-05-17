155dfae5f23a0f3e5a82e55a672543426c7e9e63
Alex <lyosha85@gmail.com>
2016-05-18 00:35:12 +0900

    [WIP]

--- lib/number_in_words.rb
+++ lib/number_in_words.rb
@@ -1,13 +1,29 @@
 class NumberInWords
+  #TODO: thirteen fourteen .. nineteen twenty can be further optimized
   ZEROTOTWENTY = %w[zero one two three four five six seven eight nine ten
                       eleven twelve thirteen fourteen fifteen sixteen seventeen
                       eighteen nineteen twenty]
+
+  #TODO: thirteen fourteen .. nineteen can be further optimized
+  MULTIPLIERS = {
+          1000 => "thousand",
+          100 => "hundred",
+          90 => "ninety",
+          80 => "eighty",
+          70 => "seventy",
+          60 => "sixty",
+          50 => "fifty",
+          40 => "forty",
+          30 => "thirty",
+  }
   def convert(number)
     number = Integer(number)
+    buffer = ""
     case number
       when 0..20
-        ZEROTOTWENTY[number]
+        return ZEROTOTWENTY[number]
       else
+      #TODO: Convert to recursive function.
         "hai"
     end
   end
